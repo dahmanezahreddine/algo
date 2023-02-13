b3aaaf9016ad569e9e106121fcfe2a60b6bbbc95
zahreddine <dahmanezahreddine@gmail.com>
2023-02-13 20:11:27 +0100

    firstcommit

---
+++ main.js
@@ -0,0 +1,13 @@
+var input = ""
+var characters = 0
+var words = 0
+var vowels = 0
+ 
+for (var i = 0; i < input.length; i++) {
+    characters += input[i]
+    
+    if (condition) {
+        
+    }
+    
+}
